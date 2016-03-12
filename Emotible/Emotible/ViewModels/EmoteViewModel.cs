04755b00789b0d18901bb27ecd1ee86dfde036db
Sam Wronski <runewake2@outlook.com>
2016-03-12 15:55:32 -0600

    Initial project setup for Emotible app.
    Setup Emotible UWP app and Emotible.Test project for Unit Testing app.

---
+++ Emotible/Emotible/ViewModels/EmoteViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotible.ViewModels
+{
+    internal class EmoteViewModel : BaseViewModel, IEmoteViewModel, ISizableControl
+    {
+        private string _name;
+        private string _text;
+        private int _width;
+        private int _height;
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                if (_name != value)
+                {
+                    _name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
