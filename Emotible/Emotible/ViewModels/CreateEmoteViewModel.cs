44bd13480bdfb0c9c1d9294d4c3b11f30bd0c9a7
Sam Wronski <runewake2@outlook.com>
2016-03-16 23:20:57 -0500

    Updated emoticon layout.
    Added a label for the number of hidden emoticons due to filter. Added quick margin to tiles.

---
+++ Emotible/Emotible/ViewModels/CreateEmoteViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotible.ViewModels
+{
+    public class CreateEmoteViewModel : BaseViewModel
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
